53422e97c667877cb06214c222c9bfa6e7bcaaf0
Dina <dina_87@mail.ru>
2022-09-03 21:45:51 +0300

    Решена дополнительная задача

---
+++ Additional task/Program.cs
@@ -0,0 +1,21 @@
+﻿// Доп. задача
+// Написать программу для разворота массива
+
+Console.WriteLine ("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if (number > 9999 && number < 100000) 
+{
+       Console.WriteLine ("Перевернутое число ");
+       Console.Write (numberText[4]);
+       Console.Write (numberText[3]);
+       Console.Write (numberText[2]);
+       Console.Write (numberText[1]);
+       Console.Write (numberText[0]);
+    }
+   
+
+ else {
+  Console.WriteLine("Введено не пятизначное число");
+}
